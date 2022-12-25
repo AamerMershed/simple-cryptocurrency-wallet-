004e97e79d09fb93a59b205125f23cd283aac45d
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2022-12-25 03:15:12 +0100

    Add files via upload

---
+++ Wallet/wall.js
@@ -0,0 +1,28 @@
+class Wallet {
+  constructor(balance) {
+    this.balance = balance;
+  }
+
+  getBalance() {
+    return this.balance;
+  }
+
+  deposit(amount) {
+    this.balance += amount;
+  }
+
+  withdraw(amount) {
+    if (amount > this.balance) {
+      console.log("Insufficient funds");
+      return;
+    }
+    this.balance -= amount;
+  }
+}
+
+const wallet = new Wallet(100);
+console.log(wallet.getBalance()); // 100
+wallet.deposit(50);
+console.log(wallet.getBalance()); // 150
+wallet.withdraw(200); // Insufficient funds
+console.log(wallet.getBalance()); // 150
