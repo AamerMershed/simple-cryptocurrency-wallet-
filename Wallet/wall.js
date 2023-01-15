36999b5d025ba51a097e48ee08e704023fa5c9db
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-01-15 12:14:48 +0100

    Update wall.js

--- Wallet/wall.js
+++ Wallet/wall.js
@@ -20,7 +20,7 @@ class Wallet {
   }
 }
 
-const wallet = new Wallet(10);
+const wallet = new Wallet(100);
 console.log(wallet.getBalance()); // 100
 wallet.deposit(50);
 console.log(wallet.getBalance()); // 150
